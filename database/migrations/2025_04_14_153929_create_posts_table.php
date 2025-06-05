9ecceae31d474247e8d5c8f5a9d7079a8d10e1c2
mcmay01 <cybehad@gmail.com>
2025-06-05 18:46:28 +0300

    updated post migration

--- database/migrations/2025_04_14_153929_create_posts_table.php
+++ database/migrations/2025_04_14_153929_create_posts_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
             $table->uuid('category_id');
             $table->foreign('category_id')
                 ->constrained()->references('id')->on('categories');
